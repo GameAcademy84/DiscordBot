15f8797e34ff8a73268f392d6610a582be6da706
Tristan GIRAULT <t.girault@GA.local>
2023-01-13 16:56:09 +0100

    test 3

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const config = require("./config.json");
 
 const { SapphireClient } = require('@sapphire/framework');
+const { Client } = require('discord.js');
 const {GatewayIntentBits } = require('discord.js');
 const path = require('path');
 
-const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers]});
+const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
+const clien2 = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
 
 // client2.registry //Création du registre des commandes disponibles NE FONCTIONNE PAS AU 11/01/23
 //     .registerDefaultTypes()
@@ -18,9 +20,9 @@ const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayI
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 const jointocreate = require("./jointocreate"); //Lancement du bot Jointocreate
-	jointocreate(client);
+	jointocreate(client2);
 const jointocreate2 = require("./jointocreate2"); //Lancement du bot Jointocreate2
-	jointocreate2(client);
+	jointocreate2(client2);
 //const welcome = require("./welcome"); //Lancement du bot Welcome
 //	welcome(client);
 
@@ -29,4 +31,6 @@ const jointocreate2 = require("./jointocreate2"); //Lancement du bot Jointocreat
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 client.on("ready", ()=>console.log("BOT Prêt")); 
 client.login(config.TOKEN); //Jointocreate & Jointocreate2
+client2.on("ready", () => console.log("client 2 Prêt"));
+client2.login(config.TOKEN); //Jointocreate & Jointocreate2
 
