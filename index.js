0b872770a8fd27f7aec4d2208d61b5012a799800
Tristan GIRAULT <t.girault@GA.local>
2023-01-16 10:59:24 +0100

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {GatewayIntentBits } = require('discord.js');
 const path = require('path');
 
 const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
-const client2 = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
+const client2 = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GUILD_VOICE_STATES] });
 
 // client2.registry //Création du registre des commandes disponibles NE FONCTIONNE PAS AU 11/01/23
 //     .registerDefaultTypes()
