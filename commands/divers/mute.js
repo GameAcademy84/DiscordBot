48d71bc1147dbc685e30dc50d8aa1e5f4605a06b
Xylcan <58815741+Xylcan@users.noreply.github.com>
2023-01-17 15:19:46 +0100

    add mute file

---
+++ commands/divers/mute.js
@@ -0,0 +1,66 @@
+const { Command } = require('@sapphire/framework');
+const { MembershipScreeningFieldType } = require('discord.js');
+
+
+module.exports = class HelloCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'mute',
+			memberName: 'mute',
+			group: 'divers',
+			aliases: ['muting', 'silence'],
+			description: 'mute a discord member',
+	                guildOnly: true,
+	                throttling: {
+	                        usages: 2,
+	                        duration: 10,
+	                },
+		});
+	}
+	registerApplicationCommands(registry)
+    {
+        registry.registerChatInputCommand((builder)=>
+        {
+            builder
+            .setName(this.name)
+            .setDescription(this.description)
+            .addUserOption(option => 
+                option
+                .setName('user')
+                .setDescription('mute the discord user')
+                .setRequired(true)
+                )
+			.addStringOption(option => 
+				option
+				.setName('reason')
+				.setDescription('reason to mute')
+				.setRequired(true)
+				)
+        })
+    }
+
+	async chatInputRun(interaction)
+	{
+		let user = interaction.options.getUser('user');
+		let member = interaction.guild.members.cache.get(user.id);
+		let muteRole = interaction.guild.roles.cache.get("933000009161523220");
+		if(member != null)
+		{
+			if (member.roles.cache.has(muteRole.id)) 
+			{
+				interaction.channel.send(member.displayName + " est déjà **mute**");
+			}
+			else
+			{
+				member.roles.add(muteRole).catch(console.error);
+				member.send("Vous avez été **mute** pour la raison suivant : " + interaction.options.getStrint('reason'));
+				await interaction.reply(member.displayName + " est désormais **muet**");
+			}
+		}
+	}
+};
+
+      //await member.roles.add('895023579484274748');
+
+
+
--- commands/divers/said.js
+++ commands/divers/said.js
@@ -46,7 +46,7 @@ module.exports = class SaidCommand extends Command {
 				option
 				.setName('image_url')
 				.setDescription('image url')
-				.setRequired(true)
+				.setRequired(false)
 			)
         })
     }
@@ -59,27 +59,15 @@ async chatInputRun(interaction)
 		.setURL(interaction.options.getString('url'))
 		.setAuthor({ name: 'GAME ACADEMY', iconURL: 'https://cdn.discordapp.com/attachments/873180067977629766/1062661853228048384/logo.png', url: 'https://www.gameacademy.fr' })
 		.setDescription(interaction.options.getString('description'))
-		.setThumbnail('https://uploads-ssl.webflow.com/5fb51d39d7860f53100a7005/62039a4881efb147f51fa5b8_FullWhite_Game%20Academy_lateral-p-500.png')//GA BLANC
-		/*.addFields(
-			{ name: 'Regular field title', value: 'Some value here' },
-			{ name: '\u200B', value: '\u200B' },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
-		)
-		.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })*/
-		.setImage(interaction.options.getString('image_url'));
-		//.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+		.setThumbnail('https://uploads-ssl.webflow.com/5fb51d39d7860f53100a7005/62039a4881efb147f51fa5b8_FullWhite_Game%20Academy_lateral-p-500.png');//GA BLANC
+		if(interaction.options.getString('image_url') !== null)
+			messageEmbed.setImage(interaction.options.getString('image_url'));
+
 
 		await interaction.channel.send({ embeds: [messageEmbed] });  
-        //await message.channel.send(Args[0] + Args[1] + Args[2] + Args[3]);  
+
             
-            
-		//message.delete();
-		//messageSlice = messageSlice.replace(/(https?:\/\/[^\s]+)/, '');
-        //await message.channel.send(messageSlice).then(message2 => message2.edit(messageSlice.replace(/(https?:\/\/[^\s]+)/, '')));
-       // const msg_attachment = message.attachment;  
-		//await message.channel.send(messageSlice).then(message2=>message2.edit();
-        //await message.edit("edition");
+        
     
 }
 
